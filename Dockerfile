7981cbf1a7acef233db0f61579c7d536d10d0792
leost <leostrukov@ya.ru>
2024-12-18 23:38:21 +0300

    Add api_data

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,24 @@
 FROM python:3
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./main.py" ]
+# Устанавливаем зависимости
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Устанавливаем supervisord для управления процессами
+RUN apt-get update && apt-get install -y supervisor && apt-get clean
+
+# Копируем конфигурацию supervisord
+COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
+# Открываем порты для API и вебхука
+EXPOSE 5000
+EXPOSE 8443
+
+# Указываем команду для запуска контейнера
+CMD ["supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
---
+++ api_data.py
@@ -0,0 +1,27 @@
+import flask
+from flask import request
+import json
+from data import db_session
+from data.StreetArt import StreetArt
+
+
+app = flask.Flask(__name__)
+blueprint = flask.Blueprint(
+    'street_art_data',
+    __name__,
+)
+
+db_session.global_init('data_base.sqlite')
+
+
+@blueprint.route('/api/arts_data', method=['GET'])
+def get_data():
+    db_session.global_init('data_base.sqlite')
+    db_sess = db_session.create_session()
+    arts = [{'id': i.id, 'longitude': i.longitude, 'latitude': i.latitude} for i in db_sess.query(StreetArt).all()]
+    return json.dumps(arts)
+
+
+if __name__ == '__main__':
+    app.register_blueprint(blueprint)
+    app.run(host='0.0.0.0', port='5000')

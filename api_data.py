fdbf06ac6737ec22ba65edf4c1f16c54bb2348ac
leost <leostrukov@ya.ru>
2024-12-19 16:41:25 +0300

    Fix bugs

--- api_data.py
+++ api_data.py
@@ -1,11 +1,12 @@
 import flask
-from flask import request
-import json
+from flask import request, jsonify
+from flask_cors import CORS
 from data import db_session
 from data.StreetArt import StreetArt
 
 
 app = flask.Flask(__name__)
+CORS(app)
 blueprint = flask.Blueprint(
     'street_art_data',
     __name__,
@@ -19,9 +20,9 @@ def get_data():
     db_session.global_init('data_base.sqlite')
     db_sess = db_session.create_session()
     arts = [{'id': i.id, 'longitude': i.longitude, 'latitude': i.latitude} for i in db_sess.query(StreetArt).all()]
-    return json.dumps(arts)
+    return jsonify(arts)
 
 
 if __name__ == '__main__':
     app.register_blueprint(blueprint)
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', ssl_context=('cert.pem', 'key.pem'))
